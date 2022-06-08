fc66357edb425fc44b6e1dbe05f70e5cb200983b
Tony Ly <tony.m.ly7@gmail.com>
2022-06-07 22:36:45 -0400

    codewars commit!!

---
+++ powersOf2.js
@@ -0,0 +1,8 @@
+function powersOfTwo(n){
+    let arr = []
+    for (i=0; i<=n; i++){
+      arr[i]=2**i
+    }
+    return arr
+  }
+//   https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/javascript
