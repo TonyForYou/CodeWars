8012ab3e2c27e3cde886590b0e66719997a8971b
Tony Ly <tony.m.ly7@gmail.com>
2022-06-16 22:57:37 -0400

    codewars

---
+++ lengthAndTwoValues.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/62a611067274990047f431a8
+function alternate(n, firstValue, secondValue){
+    //params - array size
+    // returns - array of repeated firstValue and secondValue
+    // example - 5, true, false     -->  [true, false, true, false, true]
+    // pseudo - while i<=n arr.push
+    let arr = []
+    let values = [firstValue, secondValue]
+    let i = 0
+    while (i<n){
+        arr.push(values[i%2])
+        i++
+    }
+    return arr
+  }
